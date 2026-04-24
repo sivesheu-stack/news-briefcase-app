0c7d79a1869ddd9236ad977a44b538568fb7ad76
sivesh <sivesheu@gmail.com>
2026-04-24 15:08:15 +0530

    minor hickup

--- backend/services/quizServices.js
+++ backend/services/quizServices.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export async function generateQuiz(text, level) {
     const prompt = `
-Generate 5 MCQs based on this:
+Generate 15 MCQs based on this:
 ${text}
 
 Return JSON:
